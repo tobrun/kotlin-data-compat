352ba9ca38391ae18b172ce1c8b71878ab58d509
Tobrun Van Nuland <tobrun.van.nuland@gmail.com>
2022-02-19 22:37:02 +0100

    cleanup code

--- processor/src/main/kotlin/com/tobrun/datacompat/DataCompatProcessor.kt
+++ processor/src/main/kotlin/com/tobrun/datacompat/DataCompatProcessor.kt
@@ -22,10 +22,10 @@ class DataCompatProcessor(
 ) : SymbolProcessor {
 
     override fun process(resolver: Resolver): List<KSAnnotated> {
-        logger.info("DataCompatProcessor: process")
+        logger.info("DataCompat: process")
         val annotated = resolver.getSymbolsWithAnnotation(DataCompat::class.qualifiedName!!, true)
         if (annotated.count() == 0) {
-            logger.info("No DataCompat annotations found for processing")
+            logger.info("DataCompat: No DataCompat annotations found for processing")
             return emptyList()
         }
 
@@ -36,70 +36,22 @@ class DataCompatProcessor(
 
     private inner class Visitor : KSVisitorVoid() {
 
-        private lateinit var ksType: KSType
-        private lateinit var packageName: String
-
         override fun visitClassDeclaration(classDeclaration: KSClassDeclaration, data: Unit) {
-            val qualifiedName = classDeclaration.qualifiedName?.asString() ?: run {
-                logger.error(
-                    "@DataClass must target classes with a qualified name",
-                    classDeclaration
-                )
+            if (isInvalidAnnotatedSetup(classDeclaration)) {
                 return
             }
-
-            if (!classDeclaration.isDataClass()) {
-                logger.error(
-                    "@DataClass cannot target a non-data class $qualifiedName",
-                    classDeclaration
-                )
-                return
-            }
-
-            if (!classDeclaration.isPrivate()) {
-                logger.error(
-                    "@DataClass target must have private visibility",
-                    classDeclaration
-                )
-                return
-            }
-
-            if (classDeclaration.typeParameters.any()) {
-                logger.error(
-                    "@DataClass target shouldn't have type parameters",
-                    classDeclaration
-                )
-                return
-            }
-
-            val classDeclarationName = classDeclaration.simpleName.asString()
-            if (!classDeclarationName.endsWith("Data")) {
-                logger.error(
-                    "@DataClass target must end with Data suffix naming",
-                    classDeclaration
-                )
-                return
-            }
-
-            ksType = classDeclaration.asType(emptyList())
-            packageName = classDeclaration.packageName.asString()
-
-            classDeclaration.getAllProperties()
-                .forEach {
-                    it.accept(this, Unit)
-                }
 
             // Cleanup class name by dropping Data part
             // TODO make this part more flexible with providing name inside the annotation
-            val className = classDeclarationName.dropLast(4)
+            val className = classDeclaration.simpleName.asString().dropLast(4)
+            val packageName = classDeclaration.packageName.asString()
 
-            // Align KSP properties with KoltinPoet TypeNames
-            val propertyTypeMap = mutableMapOf<KSPropertyDeclaration, TypeName>()
-            val properties = classDeclaration.getAllProperties()
-            for (property in properties) {
+            // Map KSP properties with KoltinPoet TypeNames
+            val propertyMap = mutableMapOf<KSPropertyDeclaration, TypeName>()
+            for (property in classDeclaration.getAllProperties()) {
                 val classTypeParams = classDeclaration.typeParameters.toTypeParameterResolver()
                 val typeName = property.type.resolve().toTypeName(classTypeParams)
-                propertyTypeMap[property] = typeName
+                propertyMap[property] = typeName
             }
 
             // KotlinPoet class builder
@@ -108,103 +60,129 @@ class DataCompatProcessor(
                 // Constructor
                 val constructorBuilder = FunSpec.constructorBuilder()
                 constructorBuilder.addModifiers(KModifier.PRIVATE)
-                for (entry in propertyTypeMap) {
+                for (entry in propertyMap) {
                     constructorBuilder.addParameter(entry.key.toString(), entry.value)
                 }
                 primaryConstructor(constructorBuilder.build())
 
                 // Property initializers
-                for (entry in propertyTypeMap) {
-                    addProperty(PropertySpec.builder(entry.key.toString(), entry.value)
-                        .initializer(entry.key.toString())
-                        .build())
+                for (entry in propertyMap) {
+                    addProperty(
+                        PropertySpec.builder(entry.key.toString(), entry.value)
+                            .initializer(entry.key.toString())
+                            .build()
+                    )
                 }
 
                 // Function toString
-                addFunction(FunSpec.builder("toString")
-                    .addModifiers(KModifier.OVERRIDE)
-                    .addStatement(propertyTypeMap.keys.joinToString(
-                        prefix = "return \"$className(",
-                        transform = { "$it=$$it" },
-                        postfix = ")\""
-                    ))
-                    .build()
+                addFunction(
+                    FunSpec.builder("toString")
+                        .addModifiers(KModifier.OVERRIDE)
+                        .addStatement(
+                            propertyMap.keys.joinToString(
+                                prefix = "return \"$className(",
+                                transform = { "$it=$$it" },
+                                postfix = ")\""
+                            )
+                        )
+                        .build()
                 )
 
                 // Function equals
                 val equalsStatementList = mutableListOf("other is $className")
-                for (entry in propertyTypeMap) {
+                for (entry in propertyMap) {
                     equalsStatementList.add("&& ${entry.key} == other.${entry.key}")
                 }
                 val equalsBuilder = FunSpec.builder("equals")
                     .addModifiers(KModifier.OVERRIDE)
                     .addParameter("other", ANY.copy(nullable = true))
-                    .addStatement(equalsStatementList.joinToString(
-                        prefix = "return ",
-                        separator = "\n\t\t"
-                    ))
+                    .addStatement(
+                        equalsStatementList.joinToString(
+                            prefix = "return ",
+                            separator = "\n\t\t"
+                        )
+                    )
                     .returns(Boolean::class)
                 addFunction(equalsBuilder.build())
 
                 // Function hashCode
-                addFunction(FunSpec.builder("hashCode")
-                    .addModifiers(KModifier.OVERRIDE)
-                    .addStatement(propertyTypeMap.keys.joinToString(
-                        prefix = "return Objects.hash(",
-                        separator = ", ",
-                        postfix = ")"
-                    ))
-                    .returns(Int::class)
-                    .build()
+                addFunction(
+                    FunSpec.builder("hashCode")
+                        .addModifiers(KModifier.OVERRIDE)
+                        .addStatement(
+                            propertyMap.keys.joinToString(
+                                prefix = "return Objects.hash(",
+                                separator = ", ",
+                                postfix = ")"
+                            )
+                        )
+                        .returns(Int::class)
+                        .build()
                 )
 
             }
 
             // Builder pattern
             val builderBuilder = TypeSpec.classBuilder("Builder")
-            for (entry in propertyTypeMap) {
-                val propertyName = entry.key.toString()
-                val nullableType = entry.value.copy(nullable = true)
-                builderBuilder.addProperty(PropertySpec.builder(propertyName, nullableType)
-                    .initializer(CodeBlock.builder()
-                        .add("null")
+            for (property in propertyMap) {
+                val propertyName = property.key.toString()
+                val nullableType = property.value.copy(nullable = true)
+                builderBuilder.addProperty(
+                    PropertySpec.builder(propertyName, nullableType)
+                        .initializer(
+                            CodeBlock.builder()
+                                .add("null")
+                                .build()
+                        )
+                        .addAnnotation(
+                            AnnotationSpec.builder(JvmSynthetic::class)
+                                .useSiteTarget(AnnotationSpec.UseSiteTarget.SET)
+                                .build()
+                        )
+                        .mutable()
                         .build()
-                    )
-                    .addAnnotation(
-                        AnnotationSpec.builder(JvmSynthetic::class)
-                            .useSiteTarget(AnnotationSpec.UseSiteTarget.SET)
-                            .build()
-                    )
-                    .mutable()
-                    .build())
-                builderBuilder.addFunction(FunSpec.builder("set${propertyName.capitalize()}")
-                    .addParameter(propertyName, nullableType)
-                    .addStatement("this.${propertyName} = $propertyName")
-                    .addStatement("return this")
-                    .returns(ClassName(packageName, className, "Builder"))
-                    .build()
+                )
+                builderBuilder.addFunction(
+                    FunSpec.builder("set${propertyName.capitalize()}")
+                        .addParameter(propertyName, nullableType)
+                        .addStatement("this.${propertyName} = $propertyName")
+                        .addStatement("return this")
+                        .returns(ClassName(packageName, className, "Builder"))
+                        .build()
                 )
             }
-            builderBuilder.addFunction(FunSpec.builder("build")
-                .addStatement(propertyTypeMap.keys.joinToString(
-                    prefix = "return $className(",
-                    transform = {
-                               "$it!!"
-                    },
-                    separator = ", ",
-                    postfix = ")"
-                ))
+
+            val buildFunction = FunSpec.builder("build")
+            for (property in propertyMap) {
+                if(!property.value.isNullable){
+                    buildFunction.addStatement("if (${property.key}==null) {")
+                    buildFunction.addStatement("\tthrow IllegalArgumentException(\"Null ${property.key} found when building $className.\")")
+                    buildFunction.addStatement("}")
+                }
+            }
+            buildFunction.addStatement(
+                    propertyMap.keys.joinToString(
+                        prefix = "return $className(",
+                        transform = {
+                            "$it!!"
+                        },
+                        separator = ", ",
+                        postfix = ")"
+                    )
+                )
                 .returns(ClassName(packageName, className))
-                .build()
-            )
+
+            builderBuilder.addFunction(buildFunction.build())
+
             classBuilder.addType(builderBuilder.build())
 
             // initializer function
-            val initializerFunctionBuilder = FunSpec.builder("Person")
+            val initializerFunctionBuilder = FunSpec.builder(className)
                 .returns(ClassName(packageName, className))
                 .addAnnotation(JvmSynthetic::class)
                 .addParameter(
-                    ParameterSpec.builder("initializer",
+                    ParameterSpec.builder(
+                        "initializer",
                         LambdaTypeName.get(
                             ClassName(packageName, className, "Builder"),
                             emptyList(),
@@ -214,12 +192,56 @@ class DataCompatProcessor(
                 )
                 .addStatement("return $className.Builder().apply(initializer).build()")
 
+            // File
             val fileBuilder = FileSpec.builder(packageName, className)
                 .addImport("java.util", "Objects")
                 .addType(classBuilder.build())
                 .addFunction(initializerFunctionBuilder.build())
 
             fileBuilder.build().writeTo(codeGenerator = codeGenerator, aggregating = false)
+        }
+
+        private fun isInvalidAnnotatedSetup(classDeclaration: KSClassDeclaration): Boolean {
+            val qualifiedName = classDeclaration.qualifiedName?.asString() ?: run {
+                logger.error(
+                    "@DataClass must target classes with a qualified name",
+                    classDeclaration
+                )
+                return true
+            }
+
+            if (!classDeclaration.isDataClass()) {
+                logger.error(
+                    "@DataClass cannot target a non-data class $qualifiedName",
+                    classDeclaration
+                )
+                return true
+            }
+
+            if (!classDeclaration.isPrivate()) {
+                logger.error(
+                    "@DataClass target must have private visibility",
+                    classDeclaration
+                )
+                return true
+            }
+
+            if (classDeclaration.typeParameters.any()) {
+                logger.error(
+                    "@DataClass target shouldn't have type parameters",
+                    classDeclaration
+                )
+                return true
+            }
+
+            if (!classDeclaration.simpleName.asString().endsWith("Data")) {
+                logger.error(
+                    "@DataClass target must end with Data suffix naming",
+                    classDeclaration
+                )
+                return true
+            }
+            return false
         }
     }
 
