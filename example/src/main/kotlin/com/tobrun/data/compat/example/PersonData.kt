01e65e210b0b4deb749cce2e506c31337fa161e7
Tobrun Van Nuland <tobrun.van.nuland@gmail.com>
2022-02-22 20:08:32 +0100

    Add support for generating kdoc

--- example/src/main/kotlin/com/tobrun/data/compat/example/PersonData.kt
+++ example/src/main/kotlin/com/tobrun/data/compat/example/PersonData.kt
@@ -2,5 +2,11 @@ package com.tobrun.data.compat.example
 
 import com.tobrun.datacompat.annotation.DataCompat
 
+/**
+ * Represents a person.
+ * @property name The full name.
+ * @property nickname The nickname.
+ * @property age The age.
+ */
 @DataCompat
 private data class PersonData(val name: String, val nickname: String? = null, val age: Int)
