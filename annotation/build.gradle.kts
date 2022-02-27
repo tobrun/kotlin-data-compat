52e99471562483f41e06b6a76dadc68078812b9b
Tobrun Van Nuland <tobrun.van.nuland@gmail.com>
2022-02-27 21:03:56 +0100

    [build] modularize the project by having one module for providing the annotaiton, while we have another providing the annotation processor

---
+++ annotation/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    kotlin("jvm")
+}
+
+group = "com.tobrun.datacompat"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    google()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
