3745b3c4b0922d939ed54e25b69b922407d814aa
Tobrun Van Nuland <tobrun.van.nuland@gmail.com>
2023-01-19 21:36:49 +0100

    update test

--- processor/src/test/kotlin/com/tobrun/datacompat/SimpleTestContent.kt
+++ processor/src/test/kotlin/com/tobrun/datacompat/SimpleTestContent.kt
@@ -22,10 +22,12 @@ internal val expectedSimpleTestContent = """
     ) {
       public override fun toString() = "Person(name=%name, nickname=%nickname, age=%age)"
     
-      public override fun equals(other: Any?): Boolean = other is Person
-      		&& name == other.name
-      		&& nickname == other.nickname
-      		&& age == other.age
+      public override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+        other as Person
+        return name == other.name && nickname == other.nickname && age == other.age
+      }
     
       public override fun hashCode(): Int = Objects.hash(name, nickname, age)
     
